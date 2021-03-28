3f555f5e2544b6c811b029a3122f116787b28cb4
juulrh <juulrh@gmail.com>
2021-03-28 13:05:20 +0200

    try add icon

--- main.js
+++ main.js
@@ -51,14 +51,6 @@ map.loadImage('images/flag.png', function (error, image){
         data: {
           type: 'FeatureCollection',
          features: locaties
-          //[{
-          //   type: 'Feature',
-          //   geometry: {
-          //     type: 'Point',
-          //     coordinates: [6.533258519883534, 53.17623958958595]
-          //   //  coordinates: [6.5159615333312315, 53.1797266170132]
-          //   }
-          // }]
         }
       });
       // plak de nieuwe source 'point' op de kaart in een eigen layer
@@ -68,6 +60,43 @@ map.loadImage('images/flag.png', function (error, image){
         source: 'point',
         layout: {
           'icon-image': 'flag',
+         'icon-size': 0.18
+        }
+      });
+    }
+  );
+});
+map.on('load', function () {
+
+  // laad een extern bestand
+ //map.loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Cat_silhouette.svg/400px-Cat_silhouette.svg.png', function (error, image) {
+//map.loadImage('https://upload.wikimedia.org/wikipedia/commons/5/54/Dialog-accept.svg', function (error, image) {
+map.loadImage('images/pasop.png', function (error, image){
+      // voeg image toe en noem het cat
+     map.addImage('pasop', image);
+
+      // defineer een punt in het geheugen
+      map.addSource('point', {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: [{
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [6.558267009864895, 53.17932862664539]
+            }
+          }]
+         //features: locaties
+        }
+      });
+      // plak de nieuwe source 'point' op de kaart in een eigen layer
+      map.addLayer({
+        id: 'points',
+        type: 'symbol',
+        source: 'point',
+        layout: {
+          'icon-image': 'pasop',
          'icon-size': 0.18
         }
       });
